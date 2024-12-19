22e36b1be4bbf9325c1a0cd7a4a2bf65bafe559d
akaio <Zezinhotwo@users.noreply.github.com.>
2024-12-19 07:58:29 -0300

    criada a BST balanceado recursiva

---
+++ dist/bst.js
@@ -0,0 +1,26 @@
+"use strict";
+function sortedArrayToBSTRecur(arr, start, end) {
+    if (start > end)
+        return null;
+    let mid = start + Math.floor((end - start) / 2);
+    let root = No(arr[mid]);
+    root.left = sortedArrayToBSTRecur(arr, start, mid - 1);
+    root.right = sortedArrayToBSTRecur(arr, mid + 1, end);
+    return root;
+}
+function sortedArrayToBST(arr) {
+    return sortedArrayToBSTRecur(arr, 0, arr.length - 1);
+}
+function preOrder(root) {
+    if (root == null)
+        return;
+    console.log(root.data);
+    preOrder(root.left);
+    preOrder(root.right);
+}
+function No(data) {
+    return { data, left: null, right: null };
+}
+const arr = [10, 20, 30];
+const root = sortedArrayToBST(arr);
+preOrder(root);
---
+++ src/bst.ts
@@ -0,0 +1,38 @@
+interface TreeNode {
+    data: number;
+    left: TreeNode | null;
+    right: TreeNode | null
+}
+function sortedArrayToBSTRecur(arr: number[], start: number, end: number) {
+
+    if (start > end) return null;
+
+    let mid: number = start + Math.floor((end - start )/ 2);
+
+    let root: TreeNode | null = No(arr[mid]);
+
+    root.left = sortedArrayToBSTRecur(arr, start, mid - 1);
+
+    root.right = sortedArrayToBSTRecur(arr, mid + 1, end);
+
+    return root;
+}
+
+function sortedArrayToBST(arr: number[]) {
+    return sortedArrayToBSTRecur(arr, 0, arr.length - 1);
+}
+
+function preOrder(root: TreeNode | null): void {
+    if (root == null) return;
+    console.log(root.data);
+    preOrder(root.left);
+    preOrder(root.right);
+}
+function No(data: number): TreeNode {
+    return { data, left: null, right: null };
+}
+
+
+const arr: number[] = [10, 20, 30];
+const root: TreeNode | null = sortedArrayToBST(arr);
+preOrder(root);
